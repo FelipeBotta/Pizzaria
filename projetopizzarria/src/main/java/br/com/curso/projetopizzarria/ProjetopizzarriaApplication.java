0b7c16328dc6915a893fa6e97f85f6d67488a1ca
Luis Felipe S Botta <felipeex7@gmail.com>
2024-06-18 20:13:48 -0300

    start

---
+++ projetopizzarria/src/main/java/br/com/curso/projetopizzarria/ProjetopizzarriaApplication.java
@@ -0,0 +1,13 @@
+package br.com.curso.projetopizzarria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetopizzarriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetopizzarriaApplication.class, args);
+	}
+
+}
---
+++ projetopizzarria/src/test/java/br/com/curso/projetopizzarria/ProjetopizzarriaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.curso.projetopizzarria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetopizzarriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
