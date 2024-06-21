f3c47aa573146d5e9575f8cbc872e79ecafedb99
Luis Felipe S Botta <felipeex7@gmail.com>
2024-06-21 18:20:36 -0300

    settings

---
+++ projetopizzarria/src/main/java/br/com/curso/projetopizzarria/pizza/Pizza.java
@@ -0,0 +1,10 @@
+package br.com.curso.projetopizzarria.pizza;
+
+public class Pizza {
+    private Long id;
+    private String nome;
+    private Sabor sabor;
+    private double preco;
+    private Tamanho tamanho;
+    private boolean disponivel;
+}
---
+++ projetopizzarria/src/main/java/br/com/curso/projetopizzarria/pizza/Sabor.java
@@ -0,0 +1,10 @@
+package br.com.curso.projetopizzarria.pizza;
+
+public enum Sabor {
+    CALABRESA,
+    QUATROQUEIJOS,
+    MARGARITA,
+    CHOCOATE,
+    MORANGO
+
+}
