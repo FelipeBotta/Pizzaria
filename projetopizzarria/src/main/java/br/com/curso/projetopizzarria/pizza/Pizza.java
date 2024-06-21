c4d0f5812d4d58eb98862a26278ddc28a32ef9f3
Luis Felipe S Botta <felipeex7@gmail.com>
2024-06-21 18:49:00 -0300

    Update Pizza.java

--- projetopizzarria/src/main/java/br/com/curso/projetopizzarria/pizza/Pizza.java
+++ projetopizzarria/src/main/java/br/com/curso/projetopizzarria/pizza/Pizza.java
@@ -1,10 +1,23 @@
 package br.com.curso.projetopizzarria.pizza;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "Pizza")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+
 public class Pizza {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
-    private Sabor sabor;
     private double preco;
-    private Tamanho tamanho;
     private boolean disponivel;
+    @Enumerated(EnumType.STRING)
+    private Tamanho tamanho;
+    @Enumerated(EnumType.STRING)
+    private Sabor sabor;
 }
